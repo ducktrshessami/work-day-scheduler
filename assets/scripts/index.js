ae18a0a4e951153a797350b895d11a501d11e134
ducktrshessami <jwee761@gmail.com>
2020-10-22 18:45:54 -0700

    organized page build into function

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,35 +1,35 @@
 (function() { // Enclose scope for debugging purposes
 
-    $("#currentDay").text(moment().format("dddd, MMMM Do"));
+    buildPage();
 
-    var main = $("main.container");
-    for (var i = 9; i < 18; i++) { // Make time blocks
-        let block = $(`
-            <div class="time-block row">
-                <div class="hour col-md-1">
-                    <span>` + moment().hour(i).format("hA") + `</span>
+    function buildPage() {
+        var now = moment();
+        var main = $("main.container");
+
+        $("#currentDay").text(now.format("dddd, MMMM Do")); // Set current day
+
+        for (var i = 9; i < 18; i++) { // Make time blocks
+            let block = $(`
+                <div class="time-block row">
+                    <div class="hour col-md-1">
+                        <span>` + moment().hour(i).format("hA") + `</span>
+                    </div>
+                    <textarea class="description col-md-10"></textarea>
+                    <button class="saveBtn col-md-1">
+                        <i>Save</i>
+                    </button>
                 </div>
-                <textarea class="description col-md-10"></textarea>
-                <button class="saveBtn col-md-1"></button>
-            </div>
-        `);
-        switch (simplify(i - moment().hour())) {
-            case -1: $(".description", block).addClass("past"); break;
-            case 0: $(".description", block).addClass("present"); break;
-            case 1: $(".description", block).addClass("future"); break;
-        }
-        main.append(block);
-    }
-
-    function simplify(n) {
-        if (n < 0) {
-            return -1;
-        }
-        else if (n > 0) {
-            return 1;
-        }
-        else {
-            return 0;
+            `);
+            if (i < now.hour()) { // 
+                $(".description", block).addClass("past");
+            }
+            else if (i > now.hour()) {
+                $(".description", block).addClass("future");
+            }
+            else {
+                $(".description", block).addClass("present");
+            }
+            main.append(block);
         }
     }
 
